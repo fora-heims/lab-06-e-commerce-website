0094e7b4149b96a008c9d73574e9fbdbda2f1181
fora-heims <foraheims@gmail.com>
2021-10-11 15:13:22 -0700

    Decided to use metals as the product to create objects of. Added images and created first object.

---
+++ metals.js
@@ -0,0 +1,9 @@
+export const metals = [
+    {id: '1',
+    name: 'mercury',
+    image: './assets/mercury.jpg',
+    description: 'Liquid at room temperature',
+    protons: 80,
+    price: 6},
+    {}
+]
