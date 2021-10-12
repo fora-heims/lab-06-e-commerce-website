2bd522efa4fe0f1750a93fb3dfd8f92fbf87c8be
fora-heims <foraheims@gmail.com>
2021-10-11 18:07:09 -0700

    Working loop made to render Objects into HTML elements for each Object in the metals array. CSS also added to style the page and newly created div's

---
+++ render.js
@@ -0,0 +1,22 @@
+export function renderMetal(metal) {
+    const metalDiv = document.createElement('div');
+    metalDiv.classList.add('metal-div');
+
+    const ima = document.createElement('img');
+    ima.src = metal.image;
+
+    const nam = document.createElement('h2');
+    nam.textContent = metal.name;
+
+    const des = document.createElement('p');
+    des.textContent = metal.description;
+
+    const pro = document.createElement('h3');
+    pro.textContent = `Atomic Number: ${metal.protons}`;
+
+    const pri = document.createElement('p');
+    pri.textContent = `Cost per Kilogram: $${metal.price}`;
+
+    metalDiv.append(nam, ima, des, pro, pri);
+    return metalDiv;
+}
