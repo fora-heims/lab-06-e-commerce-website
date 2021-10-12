0eecc5e6e50e1edec731bab70d3650a78748b2e0
fora-heims <foraheims@gmail.com>
2021-10-12 16:07:57 -0700

    TDD created and passing for renderLineItem() function.

--- test/DOM-render.test.js
+++ test/DOM-render.test.js
@@ -1,13 +1,13 @@
-import { renderCart } from '../cart/render-line-items.js';
+import { renderLineItem } from '../render-cart.js';
 import { metals } from '../data/metals.js';
 import { cartData } from '../data/cart-data.js';
 
 const test = QUnit.test;
 
-test('renderCart() function should update the DOM and fill the cart', (expect) => {
-    const expected = 'g';
+test('renderLineItem() function should return a tr HTML element with 4 td children', (expect) => {
+    const expected = '<tr><td>Aluminium</td><td>25 Kilograms</td><td>$15</td><td>$375</td></tr>';
     
-    const actual = renderCart(cartData, metals).outerHTML;
+    const actual = renderLineItem(cartData[2], metals[5]).outerHTML;
 
     expect.equal(actual, expected);
 });
