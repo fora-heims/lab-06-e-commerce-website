27ab9ffd36aa54fad9198c2a7eb55fd270784b28
fora-heims <foraheims@gmail.com>
2021-10-18 19:53:13 -0700

    ID's are automatically generated for add product function. Loops, if, and if else statements used to check if a product exists already.

--- admin/admin.js
+++ admin/admin.js
@@ -1,18 +1,31 @@
 import { addProduct } from '../src/add-product.js';
+import { getProducts } from '../src/get-products.js';
 
 const form = document.getElementById('new-product-form');
+let currentProducts = getProducts();
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let data = new FormData(form);
-    let newProduct = {
-        id: data.get('id'),
-        name: data.get('name'),
-        image: data.get('image'),
-        description: data.get('description'),
-        protons: data.get('protons'),
-        price: Number(data.get('price')),
-    };
-    addProduct(newProduct);
+    let exists = false;
+    for (let product of currentProducts) {
+        if (product.name === data.get('name')) {
+            exists = true;
+        }
+    }
+    if (exists) {
+        alert('Product already exists! Not Submitted');
+    } else {
+        let newProduct = {
+            id: `${currentProducts.length + 1}`,
+            name: data.get('name'),
+            image: data.get('image'),
+            description: data.get('description'),
+            protons: data.get('protons'),
+            price: Number(data.get('price')),
+        };
+        addProduct(newProduct);
+    }
     form.reset();
 });
